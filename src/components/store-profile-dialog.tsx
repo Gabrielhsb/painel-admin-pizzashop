712a53003763cfac216530c155768fd6ba3a0128
Gabriel Henrique <gabriel@MacBook-Air-de-Gabriel.local>
2024-06-11 15:08:05 -0300

    Implementado busca e paginação

--- src/components/store-profile-dialog.tsx
+++ src/components/store-profile-dialog.tsx
@@ -1,14 +1,16 @@
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-import { updateProfile } from "@/api/update-profile";
-import {
-    getManagedRestaurant,
-    GetManagedRestaurantResponse,
-  } from '@/api/get-managed-restaurant'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+import { z } from 'zod'
 
-import { Button } from "./ui/button";
+import {
+  getManagedRestaurant,
+  GetManagedRestaurantResponse,
+} from '@/api/get-managed-restaurant'
+import { updateProfile } from '@/api/update-profile'
+
+import { Button } from './ui/button'
 import {
   DialogClose,
   DialogContent,
@@ -16,26 +18,26 @@ import {
   DialogFooter,
   DialogHeader,
   DialogTitle,
-} from "./ui/dialog";
-import { Input } from "./ui/input";
-import { Label } from "./ui/label";
-import { Textarea } from "./ui/textarea";
-import { toast } from "sonner";
-import { queryClient } from "@/lib/react-query";
+} from './ui/dialog'
+import { Input } from './ui/input'
+import { Label } from './ui/label'
+import { Textarea } from './ui/textarea'
 
 const storeProfileSchema = z.object({
   name: z.string().min(1),
-  description: z.string(),
-});
+  description: z.string().nullable(),
+})
 
-type StoreProfileSchema = z.infer<typeof storeProfileSchema>;
+type StoreProfileSchema = z.infer<typeof storeProfileSchema>
 
 export function StoreProfileDialog() {
+  const queryClient = useQueryClient()
+
   const { data: managedRestaurant } = useQuery({
-    queryKey: ["managed-restaurant"],
+    queryKey: ['managed-restaurant'],
     queryFn: getManagedRestaurant,
     staleTime: Infinity,
-  });
+  })
 
   const {
     register,
@@ -44,43 +46,60 @@ export function StoreProfileDialog() {
   } = useForm<StoreProfileSchema>({
     resolver: zodResolver(storeProfileSchema),
     values: {
-      name: managedRestaurant?.name ?? "",
-      description: managedRestaurant?.description ?? "",
+      name: managedRestaurant?.name ?? '',
+      description: managedRestaurant?.description ?? '',
     },
-  });
+  })
 
   const { mutateAsync: updateProfileFn } = useMutation({
     mutationFn: updateProfile,
-    onSuccess(_, { description, name }) {
-        const cached = queryClient.getQueryData<GetManagedRestaurantResponse>([
-          'managed-restaurant',
-        ])
-  
-        if (cached) {
-          queryClient.setQueryData<GetManagedRestaurantResponse>(
-            ['managed-restaurant'],
-            {
-              ...cached,
-              name,
-              description,
-            },
-          )
-        }
-      },
-  });
+    onMutate({ description, name }) {
+      const { cached } = updateManagedRestaurantCache({ description, name })
+
+      return { previousProfile: cached }
+    },
+    onError(_, __, context) {
+      if (context?.previousProfile) {
+        updateManagedRestaurantCache(context.previousProfile)
+      }
+    },
+  })
+
+  function updateManagedRestaurantCache({
+    name,
+    description,
+  }: StoreProfileSchema) {
+    const cached = queryClient.getQueryData<GetManagedRestaurantResponse>([
+      'managed-restaurant',
+    ])
+
+    if (cached) {
+      queryClient.setQueryData<GetManagedRestaurantResponse>(
+        ['managed-restaurant'],
+        {
+          ...cached,
+          name,
+          description,
+        },
+      )
+    }
+
+    return { cached }
+  }
 
   async function handleUpdateProfile(data: StoreProfileSchema) {
     try {
       await updateProfileFn({
         name: data.name,
         description: data.description,
-      });
+      })
 
-      toast.success("Perfil atualizado com sucesso!");
+      toast.success('Perfil atualizado com sucesso!')
     } catch {
-      toast.error("Falha ao atualizar o perfil, tente novamente");
+      toast.error('Falha ao atualizar o perfil, tente novamente')
     }
   }
+
   return (
     <DialogContent>
       <DialogHeader>
@@ -96,7 +115,7 @@ export function StoreProfileDialog() {
             <Label className="text-right" htmlFor="name">
               Nome
             </Label>
-            <Input className="col-span-3" id="name" {...register("name")} />
+            <Input className="col-span-3" id="name" {...register('name')} />
           </div>
 
           <div className="grid grid-cols-4 items-center gap-4">
@@ -106,7 +125,7 @@ export function StoreProfileDialog() {
             <Textarea
               className="col-span-3"
               id="description"
-              {...register("description")}
+              {...register('description')}
             />
           </div>
         </div>
@@ -123,5 +142,5 @@ export function StoreProfileDialog() {
         </DialogFooter>
       </form>
     </DialogContent>
-  );
+  )
 }
